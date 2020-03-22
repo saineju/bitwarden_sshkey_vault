e1c6fa4e91986166821d49f700dcaee44a017880
saineju <jussi.saine@ixonos.com>
2020-03-22 22:24:30 +0200

    Initial import

--- README.md
+++ README.md
@@ -1,2 +1,72 @@
-# bitwarden_sshkey_vault
-Bitwarden SSH key storage
+# Bitwarden SSH key storage
+
+Decided to test if [bitwarden](https://bitwarden.com/) could be used as a centralized encrypted storage for SSH keys, so created a wrapper script for generating & fetching SSH keys
+to/from bitwarden vault. The idea is that the SSH key would never recide on the machine disk, but is created by using named pipe and then inputted to bitwarden
+for safe keeping.
+
+The script here is a quite simple bash script, there might be better ways to achieve this, but I decided to take the easy way out and just wrap the bitwarden 
+cli with this script. I've tested the script to be somewhat functioning in MacOS Mojave and Ubuntu, but as always there might be issues
+
+## Prerequisites
+
+To use this you will need to have following packages installed
+* bitwarden account
+* bitwarden cli (https://github.com/bitwarden/cli)
+* jq
+* ssh tools
+
+## Usage
+
+```
+Usage: ./bw_ssh_key.sh <list|generate|get_key|get_public_key> [-k key_name] [-t ttl] [-e encryption_type]
+	list	List keys in vault
+	generate	Generate new key to vault
+	get_key	        Get private key to ssh-agent
+	get_public_key	get public key for the specified key
+	-k|--key-name	Name for key, required for generating key or getting the key
+	-i|-id		Use key ID to fetch the key
+	-t|--ttl	How long private key should exist in agent, uses ssh-agent ttl syntax
+	-e|--key-enc	Key type, accepts rsa or ed25519
+	All required parameters will be asked unless specified with switch
+```
+
+The script currently allows for 
+* key creation
+
+   Key creation requires both key name and key encryption type. The key name will be prefixed with string `bw_ssh_` to allow easier listing of the items
+   but when entering the key name you may omit that as it will be added automatically. 
+
+   For the encryption alorithm you may choose either RSA or ED25519. The RSA keys will always be 4096 bit keys, ED25519 keys will use the fixed length
+   ssh-keygen uses for them.
+
+   both key name and encryption alogrithm will be asked if they are not specified with command line switches
+   
+   The keys will be added as secure notes and the note will contain both private and public key. The generation does not allow for additional password
+   to be added for the key as the idea is that the bitwarden vault encryption would suffice.
+
+* fetching the key to ssh-agent
+
+  When fetching the key to ssh-agent you need to specify the key name or key ID. As with creation, the prefix `bw_ssh_` for key name will be automatically added, so you can
+  omit that. You may also specify a time to live value for the key, if you wish to change the default one hour.
+
+* printing out public key
+  
+  Similarly to fetching the private key, you need to specify key name and you may omit the `bw_ssh_` prefix. This command does not need or use other
+  switches.
+
+* listing keys in the vault
+  Lists all entries in the vault that begin with the prefix `bw_ssh_`
+
+### Login / unlock
+
+If you're using bitwarden cli for the first time on your machine the script will attempt to do a `login` -action when it is run for the first time.
+For the consecutive runs it will check if you have already logged in and if so, it will only do an `unlock` -action for the vault. If specify the 
+BW_SESSION -environment variable and the script will use that and will not ask for password. The script will also do a `sync` action with every run
+to ensure that all keys are available.
+
+### Known issues
+
+When fetching keys from the vault, the script does a `get` action and that requires that the key name is unique enough. This means that if you have keys
+`my_key` and `my_key2`, you can fetch `my_key2`, but fetching `my_key` will fail due to the fact that the search for it will find two entries for `my_key`
+As a workaround, you should attempt to use names that will not collide like that. If you do have two colliding keys, you can always use the key ID to 
+fetch the key, but you need to know which ID is the correct one.
---
+++ bw_ssh_key.sh
@@ -0,0 +1,188 @@
+#!/bin/bash
+
+set -e
+
+key_prefix="bw_ssh_"
+ttl="1h"
+error=0
+red='\033[0;31m'
+yellow='\033[0;33m'
+nc='\033[0m'
+
+function help() {
+  echo "Usage: $0 <list|generate|get_key|get_public_key> [-k key_name] [-t ttl] [-e encryption_type]"
+  echo -e "\tlist\tList keys in vault"
+  echo -e "\tgenerate\tGenerate new key to vault"
+  echo -e "\tget_key\t\tGet private key to ssh-agent"
+  echo -e "\tget_public_key\tget public key for the specified key"
+  echo -e "\t-k|--key-name\tName for key, required for generating key or getting the key"
+  echo -e "\t-i|-id\t\tUse key ID to fetch the key"
+  echo -e "\t-t|--ttl\tHow long private key should exist in agent, uses ssh-agent ttl syntax"
+  echo -e "\t-e|--key-enc\tKey type, accepts rsa or ed25519"
+  echo -e "\tAll required parameters will be asked unless specified with switch"
+}
+
+function generate_key() {
+  tempdir=$(mktemp -d)
+  mkfifo ${tempdir}/key
+
+  while true
+  do
+    if [ -z "${key_type}" ]; then
+      echo "Please provide key type, allowed values are rsa and ed25519:"
+      read key_type
+    fi
+
+    if [ "${key_type}" == "rsa" ] || [ "${key_type}" == "ed25519" ]; then
+      break
+    else
+      unset key_type
+    fi
+  done
+  if [ "${key_type}" == "rsa" ]; then
+    ssh-keygen -t rsa -b 4096 -f ${tempdir}/key -N ''>/dev/null 2>&1 <<< y > /dev/null&
+  else
+    ssh-keygen -t ed25519 -f ${tempdir}/key -N ''>/dev/null 2>&1 <<< y > /dev/null&
+  fi
+  private_key=''
+  while read line; do
+    if [ "$line" ]; then
+      private_key+="$line\n"
+    else
+      break
+    fi
+  done < ${tempdir}/key
+  public_key=$(cat ${tempdir}/key.pub)
+  if [ -z "${key_name}" ]; then
+    echo -n "Enter name for the key: "
+    read key_name
+  fi
+  key_name=${key_name#"$key_prefix"}
+  key_name="${key_prefix}${key_name}"
+  payload="${private_key}\n${public_key}"
+  rm -f ${tempdir}/key
+  rm -f ${tempdir}/key.pub
+  rmdir ${tempdir}
+  response=$(echo "{\"organizationId\":null,\"folderId\":null,\"type\":2,\"name\":\"${key_name}\",\"notes\":\"${payload}\",\"favorite\":false,\"login\":null,\"secureNote\":{\"type\":0},\"card\":null,\"identity\":null}"|bw encode|bw create item)
+  if [ $? == 0 ]; then
+    echo -n "Created new item: "
+    echo ${response}|jq .id
+    echo -n "Item name: "
+    echo ${response}|jq .name
+    echo "Public key: ${public_key}"
+  fi
+}
+
+function list_keys() {
+  bw list items --search ${key_prefix}|jq .[].name
+}
+
+function get_item() {
+  if [ -n "${key_id}" ]; then
+    key_name="${key_id}"
+  elif [ -z "${key_name}" ]; then
+    echo -n "Enter searched key name: "
+    read key_name
+  fi
+  if [ -z "${key_id}" ]; then
+    key_name=${key_name#"$key_prefix"}
+    key_name="${key_prefix}${key_name}"
+  fi
+  result=$(bw get item ${key_name})
+  if [ $? == 0 ]; then
+    return
+  else
+    echo "You need to be more specific on the key name, the key you entered matches this list:"
+    bw list items --search ${key_name}|jq .[].name
+  fi
+
+}
+
+command -v bw >/dev/null 2>&1 || { echo -e >&2 "${red}ERROR: ${yellow}bitwarden cli${nc} is required, but not installed";error=1; }
+command -v jq >/dev/null 2>&1 || { echo -e >&2 "${red}ERROR: ${yellow}jq${nc} is required, but not installed";error=1; }
+if [ "${error}" == 1 ]; then
+  echo "Please fix errors above, aborting"
+  exit 1
+fi
+
+if [ $# -eq 0 ]; then
+  help
+  exit 1
+fi
+
+while [[ $# -gt 0 ]]
+  do
+    opt="$1"
+    case $opt in
+      -k|--key-name)
+        key_name="$2"
+        shift
+        shift
+      ;;
+      -i|-id)
+        key_id="$2"
+        shift
+        shift
+      ;;
+      -t|--ttl)
+        ttl="$2"
+        shift
+        shift
+      ;;
+      -e|--key-enc)
+        key_type="$2"
+        shift
+        shift
+      ;;
+      -h|--help)
+        help
+        exit 0
+      ;;
+      get_public_key)
+        get_public_key=true
+        shift
+      ;;
+      get_key)
+        get_key=true
+        shift
+      ;;
+      list)
+        list=true
+        shift
+      ;;
+      generate)
+        generate=true
+        shift
+      ;;
+      *)
+        echo "Unrecognized param: $1"
+        shift
+      ;;
+  esac
+done
+
+## Check login status
+bw login --check >/dev/null 2>&1
+if [ $? != 0 ]; then
+  bw login
+fi
+
+## Check for session variable
+if [ -z "${BW_SESSION}" ]; then
+  export BW_SESSION=$(bw unlock --raw)
+fi
+
+## Sync vault
+bw sync
+
+if [ "${generate}" == "true" ]; then
+  generate_key
+elif [ "${list}" == "true" ]; then
+  list_keys
+elif [ "${get_key}" == "true" ]; then
+  get_item
+  echo ${result}|jq -r '.notes'|grep -Ev "ssh-(rsa|ed25519)"|ssh-add -t ${ttl} -
+elif [ "${get_public_key}" == "true" ]; then
+  get_item
+  echo $result|jq -r '.notes'|grep -E "ssh-(rsa|ed25519)"
+fi
